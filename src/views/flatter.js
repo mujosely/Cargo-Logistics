33ab9adc06ffa6284503e045e3f75748b2012550
alice karemera <karemeraalice@gmail.com>
2022-04-01 14:14:05 +0200

    final

---
+++ src/views/flatter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import"./about.css"
+   
+
+const Flatter =() =>{
+    return(
+         <div className="flatter_container">
+             <div className="flatter_container1">
+             </div>
+             <div className="flatter_container2">
+             </div>
+             <div className="flatter_container3">
+             </div>
+             </div>
+    )
+}
+export default Flatter;
